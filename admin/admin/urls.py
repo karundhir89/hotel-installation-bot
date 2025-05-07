368730798d29bb9f1057284386a4ff19b1d054cd
Wahid-Infi <wahid@infiwebsolutions.com>
2025-05-07 13:53:24 +0530

    Fixed Issue

--- admin/admin/urls.py
+++ admin/admin/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 urlpatterns = [
     path("admin/", include("django.contrib.auth.urls")),
     path('', include('admin_dashboard.urls')),
-    path('', include('hotel_bot_app.urls'))
+    path('', include('hotel_bot_app.urls')),
+    # path('__debug__/', include('debug_toolbar.urls')),
 ]
